45282f935003b4e3293ceafb16f6bcc8d1c790ac
Vivi <sz.vivien02@gmail.com>
2020-12-08 00:36:28 +0100

    some js

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
-const task = document.getElementById('task').value;
+const task = document.getElementById('task');
 const button = document.getElementById('button');
 const pTag = document.getElementById('noText');
 const allDoneText = document.getElementById('allDoneText');
 const listContainer = document.getElementById('list-container');
+//const checkmark = &#9745;;
 
-button.addEventListener('click', addTaskByClick);
-button.addEventListener('keypress', addTaskByEnter);
+button.addEventListener('click', addTask);
+window.addEventListener('keydown', addTaskByEnter);
+task.addEventListener('click', removePtag);
+task.addEventListener('keydown', removePtagByEnter);
 
-function noInput(){
-  if(task === ''){
-    pTag.style.visibility = visible;
+function removeItem(event){
+    event.target.remove();
+    if(listContainer.childElementCount === 1){
+      allDoneText.classList.remove('none');
+    }
+}
+
+function addTask(){
+  if(task.value == ''){
+    pTag.classList.remove("none");
+    return;
+  }
+    let listitem = document.createElement('li');
+    listitem.addEventListener('click', removeItem);
+
+    if(task.value.includes('!')){
+      listitem.classList.add("urgent");
+    }
+
+    listitem.innerHTML = `<span id='checkmark'>&#9745;</span> ${task.value}`;
+    listContainer.appendChild(listitem);
+    allDoneText.classList.add("none");
+    task.value = '';
+}
+function addTaskByEnter(event){
+  if (event.keyCode === 13){
+    addTask();
   }
 }
-noInput();
-
-
-
-function addTaskByClick(){
-    let listitem = document.createElement('li');
-    listitem.innerHTML = `<span id='checkmark'>&#9745;</span> ${task}`;
-    listContainer.appendChild(listitem);
+function removePtag(){
+  pTag.classList.add('none');
 }
-
+function removePtagByEnter(event){
+  if(event.keyCode === 13){
+     removePtag();
+  }
+}
