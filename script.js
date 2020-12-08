966c9a931703ed884f8c626cda227e63fec4c8ad
Vivi <sz.vivien02@gmail.com>
2020-12-08 21:51:45 +0100

    Ready - checkbox

--- script.js
+++ script.js
@@ -3,18 +3,20 @@ const button = document.getElementById('button');
 const pTag = document.getElementById('noText');
 const allDoneText = document.getElementById('allDoneText');
 const listContainer = document.getElementById('list-container');
-//const checkmark = &#9745;;
 
 button.addEventListener('click', addTask);
 window.addEventListener('keydown', addTaskByEnter);
-task.addEventListener('click', removePtag);
-task.addEventListener('keydown', removePtagByEnter);
+task.addEventListener('input', removePtag);
 
 function removeItem(event){
     event.target.remove();
     if(listContainer.childElementCount === 1){
       allDoneText.classList.remove('none');
     }
+}
+
+function removePtag(){
+  pTag.classList.add('none');
 }
 
 function addTask(){
@@ -28,8 +30,15 @@ function addTask(){
     if(task.value.includes('!')){
       listitem.classList.add("urgent");
     }
-
-    listitem.innerHTML = `<span id='checkmark'>&#9745;</span> ${task.value}`;
+    function letShorten(cucc){
+      if(cucc.length >= 62){
+        let shortToDo = cucc.slice(0, 59);
+        return `${shortToDo}...`;
+      }
+      return cucc;
+    }
+    listitem.setAttribute('title', `${task.value}`);
+    listitem.innerHTML = `<span id='checkmark'>&#9745;</span> ${letShorten(task.value)}`;
     listContainer.appendChild(listitem);
     allDoneText.classList.add("none");
     task.value = '';
@@ -37,13 +46,5 @@ function addTask(){
 function addTaskByEnter(event){
   if (event.keyCode === 13){
     addTask();
-  }
-}
-function removePtag(){
-  pTag.classList.add('none');
-}
-function removePtagByEnter(event){
-  if(event.keyCode === 13){
-     removePtag();
   }
 }
